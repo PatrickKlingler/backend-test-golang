bfe483e01be8f1fb5480e3fa904c04b12fe4d9a6
Patrick Klingler <patrickklingler@utexas.edu>
2023-07-18 23:54:52 +0200

    Tweaks to successfully return static list of facts

--- backend-service-gin/main.go
+++ backend-service-gin/main.go
@@ -1,52 +1,35 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
+	"context"
+	"fmt"
+	"log"
 
 	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // You will be using this Trainer type later in the program
 type fact struct {
-    Text string
-    Number  int
-    Found bool
-	Type string
+	Text   string
+	Number int
+	Found  bool
+	Type   string
 }
 
 // albums slice to seed record album data.
 var facts = []fact{
-    {Text: "83 is the atomic number of bismuth.", Number: 83, Found: true, type: "trivia"},
-    {Text: "102 is the atomic number of nobelium, an actinide.", Number: 102, Found: true, type: "trivia"},
+	{Text: "83 is the atomic number of bismuth.", Number: 83, Found: true, Type: "trivia"},
+	{Text: "102 is the atomic number of nobelium, an actinide.", Number: 102, Found: true, Type: "trivia"},
 }
 
 // getFacts responds with the list of facts as JSON.
 func getFacts(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, facts)
-}
-
-// getFactByNumber locates the fact whose Number value matches the number
-// parameter sent by the client, then returns that fact as a response.
-func getAlbumByID(c *gin.Context) {
-	number := c.Param("number")
-
-    // Loop through the list of facts, looking for
-    // an album whose number value matches the parameter.
-    for _, f := range facts {
-        if f.Number == number {
-            c.IndentedJSON(http.StatusOK, f)
-            return
-        }
-    }
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "fact not found"})
+	c.IndentedJSON(http.StatusOK, facts)
 }
 
 func main() {
@@ -70,8 +53,7 @@ func main() {
 	fmt.Println("Connected to MongoDB!")
 
 	router := gin.Default()
-    router.GET("/facts", getFacts)
-    router.GET("/facts/:number", getFactByNumber)
+	router.GET("/facts", getFacts)
 
-    router.Run("localhost:8080")
+	router.Run("localhost:8080")
 }
